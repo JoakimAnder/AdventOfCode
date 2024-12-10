9a1071f9cfc8a4bb95271a4e58678d5ffbc1ee40
Joakim Andersson <Joakim.Ander@hotmail.com>
2024-12-10 21:04:47 +0100

    2024-5

---
+++ AdventOfCode.Solutions/Puzzles/Year2024/Day05/OneStarSolution.cs
@@ -0,0 +1,66 @@
+﻿using AdventOfCode.Solutions.Helpers;
+using AdventOfCode.Solutions.Models;
+using System.Collections.Frozen;
+
+namespace AdventOfCode.Solutions.Puzzles.Year2024.Day05;
+
+public class OneStarSolution : ISolution
+{
+    public ValueTask<object> Solve(string input, CancellationToken ct)
+    {
+        var sum = 0;
+
+        var (orderingRules, pages) = ParseInput(input);
+
+        foreach (var page in pages)
+        {
+            if (!IsInCorrectOrder(page, orderingRules))
+                continue;
+
+            var middlePage = page.ElementAt(page.Count / 2);
+            sum += middlePage;
+        }
+
+        return ValueTask.FromResult<object>(sum);
+    }
+
+    internal static bool IsInCorrectOrder(IReadOnlyCollection<int> page, IReadOnlyCollection<Point2D> orderingRules)
+    {
+        var indexedPage = page.Index().ToFrozenDictionary(t => t.Item, t => t.Index);
+        foreach (var rule in orderingRules)
+        {
+            if (!indexedPage.TryGetValue(rule.X, out var x) || !indexedPage.TryGetValue(rule.Y, out var y))
+                continue;
+
+            var isWrongOrder = x >= y;
+            if (isWrongOrder)
+                return false;
+        }
+
+        return true;
+    }
+
+    internal static (IReadOnlyCollection<Point2D> OrderingRules, IReadOnlyCollection<IReadOnlyCollection<int>> Pages) ParseInput(ReadOnlySpan<char> input)
+    {
+        List<Point2D> orderingRules = [];
+        List<IReadOnlyCollection<int>> pages = [];
+
+        foreach (var line in input.EnumerateLines())
+        {
+            if (line.Contains(','))
+            {
+                pages.Add(line.GetIntegers());
+                continue;
+            }
+
+            if (line.Contains('|'))
+            {
+                var ints = line.GetIntegers();
+                orderingRules.Add((ints.First(), ints.Last()));
+            }
+
+        }
+
+        return (orderingRules, pages);
+    }
+}
---
+++ AdventOfCode.Solutions/Puzzles/Year2024/Day05/TwoStarSolution.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Solutions.Models;
+using static AdventOfCode.Solutions.Puzzles.Year2024.Day05.OneStarSolution;
+
+namespace AdventOfCode.Solutions.Puzzles.Year2024.Day05;
+
+public class TwoStarSolution : ISolution
+{
+    public ValueTask<object> Solve(string input, CancellationToken ct)
+    {
+        var sum = 0;
+
+        var (orderingRules, pages) = ParseInput(input);
+        var pageComparer = new PageComparer(orderingRules);
+        foreach (var page in pages)
+        {
+            var currentPage = page;
+            if (IsInCorrectOrder(page, orderingRules))
+                continue;
+
+            var middlePage = page.Order(pageComparer).ElementAt(page.Count / 2);
+            sum += middlePage;
+        }
+
+        return ValueTask.FromResult<object>(sum);
+    }
+
+    private class PageComparer(IReadOnlyCollection<Point2D> orderingRules) : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            var rule = orderingRules.First(r => (r.X, r.Y) == (x, y) || (r.X, r.Y) == (y, x));
+
+            if (x == rule.X)
+                return -1;
+
+            return 1;
+        }
+    }
+}
